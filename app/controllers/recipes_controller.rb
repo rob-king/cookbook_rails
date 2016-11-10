04aaf43467467b7bd0feac456d3725d82254204d
robert <robert.k25@gmail.com>
2016-11-09 22:03:07 -0500

    adds controller actions for index, show, and new views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,16 @@
 class RecipesController < ApplicationController
   def new
+    @recipe = Recipe.new
   end
 
   def edit
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def index
+    @recipes =  Recipe.all
   end
 end
