ca023ddd50d648e6cee3a19216e1000ce90df9d9
robert <robert.k25@gmail.com>
2016-11-09 22:45:15 -0500

    adds controller functionality and views for editing recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,19 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
   def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to @recipe
   end
 
   def show
@@ -12,5 +24,11 @@ class RecipesController < ApplicationController
 
   def index
     @recipes =  Recipe.all
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
   end
 end
